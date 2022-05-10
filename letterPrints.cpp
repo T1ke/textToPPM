da9d3f34b05b543ced0f501d516045b48578651f
Patrik <patrik.tirkkonen@tuni.fi>
2022-05-10 14:00:26 +0300

    initial commit

---
+++ letterPrints.cpp
@@ -0,0 +1,139 @@
+#include <fstream>
+
+// 5x5 blocks
+namespace letterPrints
+{
+void A(std::ofstream& image)
+{
+    for (int i = 0; i < 5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(i == 1 && j == 2 ||
+               i == 2 && j == 1 ||  i == 2 && j == 3 ||
+                i == 3 && j == 1 || i == 3 && j == 2  || i == 3 && j == 3 ||
+                i == 4 && j == 0 ||  i == 4 && j == 4)
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+    }
+}
+
+void B(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(i == 0 && j==1 || i == 0 && j==2 ||
+                i==1 && j==1 || i==1 && j ==3 ||
+                i==2 && j == 1 || i == 2 && j ==2 ||
+                i == 3 && j == 1 || i == 3 && j == 3 ||
+                i == 4 && j == 1 || i == 4 && j==2)
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+
+void C(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(i==0 && j==1 || i == 0 && j ==2 ||
+                i == 1 && j == 0 || i == 1 && j ==3 ||
+                i == 2 && j == 0 ||
+                i == 3 && j == 0 || i == 3 && j==3 ||
+                i == 4 && j == 1 || i == 4 && j ==2)
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void D(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j==0 ||
+                i == 0 && j==2 || i == 0&& j==1 ||
+             i==1 && j ==3  ||
+             i == 2 && j ==3 ||
+             i == 3 && j == 3 ||
+            i == 4 && j==1 || i==4 && j==2)
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+
+void E(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if (j == 1 ||
+                i == 0 && j == 2 || i == 0 && j==3 ||
+                i == 2 && j==2  ||
+                i == 4 && j == 2 || i == 4 && j==3)
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+
+void F(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j == 1 ||
+                i == 0 && j == 2 || i == 0 && j==3 ||
+                i == 2 && j==2
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+}
---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <string>
+#include "letterPrints.cpp"
+
+
+std::string pathStart = "/home/tike/projects/cpp/Qt/TestProject1/Images/";
+
+bool loadImage(std::string fileName)
+{
+    std::string path = pathStart+fileName;
+    std::ifstream image(path, std::ios::binary);
+    if(image.fail())
+    {
+        std::cout << "no such file found: " << path << std::endl;
+        return false;
+
+    }
+
+    std::cout << "file found!" << std::endl;
+    image.open(path, std::ios::binary);
+    return true;
+}
+
+void writeTextToImage(std::ofstream& image, std::string input)
+{
+    // do in 5x5 blocks
+    for (int i = 0 ; i < 45; i++)
+    {
+        for(int j = 0; j < 255;j++)
+        {
+            image << "250" << " " << "250" << " " << "250" << std::endl;
+
+        }
+    }
+    letterPrints::F(image);
+
+}
+
+std::ofstream writeImageFile(std::string fileName)
+{ //makes .ppm image file with black background
+    std::ofstream image;
+    std::string path = pathStart + fileName + ".ppm";
+    image.open(path);
+
+    if(image.is_open())
+    {
+        image << "P3" << std::endl; // ppm rgb :P3" means this is a RGB color image in ASCII : wikipedia
+        image << "255 255" << std::endl; //height weight
+        image << "255" << std::endl; //max value for each color
+        for(int y = 0; y < 205 ; y++)
+        {
+            for (int x = 0; x < 255; x++)
+            {
+                image << "0" << " " << "0" << " " << "0" << std::endl;
+            }
+        }
+    }
+    //image.close();
+    return image;
+}
+
+int main()
+{
+    std::string fileName;
+    std::string input;
+    std::ofstream image;
+    std::cout << "Give me the filename to your image: " ;
+    std::getline(std::cin,fileName);
+    std::cout <<"Give me the text you want to insert into the image: ";
+    std::getline(std::cin,input);
+    image = writeImageFile(fileName);
+
+    if(input.length() != 0)
+    {
+        writeTextToImage(image,input);
+    }
+   // std::string path = pathStart + fileName;
+    image.close();
+
+    return 0;
+}
+
