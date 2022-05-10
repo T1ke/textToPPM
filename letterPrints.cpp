6f9a6f6d0e271324b7dd6f8f598243aa526fe85c
Patrik <patrik.tirkkonen@tuni.fi>
2022-05-10 16:52:42 +0300

    addeletterprinbts until N

--- letterPrints.cpp
+++ letterPrints.cpp
@@ -136,4 +136,172 @@ void F(std::ofstream& image)
         }
      }
 }
+
+void G(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(i==0 && j == 1 || i == 0 && j==2 || i==0 && j==3 ||
+                i==1 && j ==0 ||
+                i == 2 && j == 0 || i == 2 && j==2 || i ==2 && j==3 ||
+                i == 3 && j == 0 || i == 3 && j == 3 ||
+                i == 4 && j == 1 || i == 4 && j ==2
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void H(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 1 ||
+                j %5== 3 ||
+                    j%5 == 2 && i == 2
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void I(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 3
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void J(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 2 && i%5 != 4 || i%5 == 4 && j%5 == 1
+                    || j%5 == 0 && i%5 == 3
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void K(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 1 || i%5 == 0 && j%5 == 4
+                    || j%5 == 3 && i%5 == 1||
+                     j%5 == 3 && i%5 == 3 ||
+                     j%5 == 4 && i%5 == 4 || j%5==2 && i==2
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void L(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 1 ||
+                    i%5==4 && j%5 >=2 && j%5!=4
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+
+void M(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 0 || j%5 == 4
+                  || (j%5 == 2 && i%5 == 2) ||
+                    j%5 == 1 && i%5 == 1 ||
+                    j%5 == 3 && i%5 == 1
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
+void N(std::ofstream& image)
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int j=0;j<255;j++)
+        {
+            if(j%5 == 0 || j%5 == 4
+                  || (j%5 == 2 && i%5 == 2) ||
+                    j%5 == 1 && i%5 == 1 ||
+                    j%5 == 3 && i%5 == 3
+                )
+            {
+                image << "0 "<< "0 " << "0" << std::endl;
+            }
+            else
+            {
+                image << "255 "<< "255 " << "255" << std::endl;
+            }
+        }
+     }
+}
 }
